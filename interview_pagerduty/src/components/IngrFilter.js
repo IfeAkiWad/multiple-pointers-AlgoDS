7455e82e3e8feeb434b513e5dc20de3dc9c5ea40
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-08-17 12:34:21 -0500

    build search filter

---
+++ interview_pagerduty/src/components/IngrFilter.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+const IngrFilter = ({ dish }) => {
+    // console.log(dish, "dish")
+    const [filter, setFilter] = useState('')
+
+    let dishes = dish.map(dish => {
+        return dish
+     }) 
+    console.log(dishes, "dishes")
+
+    let filterIngr = dishes.filter(dish => {
+        if(filter !== '') {
+            return dish.toLowerCase().includes(filter)
+        }
+    })
+
+    const handleOnChangeFilter = (event) => {
+        setFilter(event.target.value)
+        console.log(filter)
+    }
+
+    return (
+        <div>
+            <b>search ingredients: </b><input type="text" onChange={handleOnChangeFilter} value={filter}/> 
+
+            {filterIngr.map(d => {
+                return <FilterDisplay key={d.id} dish={dish} />
+            })}
+        </div>
+    )
+}
+
+export default IngrFilter
